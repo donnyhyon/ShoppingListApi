83c98668d6be7850607ac3f51819b1aadbc125a0
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-01 08:38:44 +0000

    added item model

---
+++ src/main/java/com/example/shoppinglist/model/Item.java
@@ -0,0 +1,8 @@
+package com.example.shoppinglist.model;
+
+public class Item {
+    private Integer id;
+    private String name;
+    private Float quantity;
+    private MeasuringUnit unit;
+}
---
+++ src/main/java/com/example/shoppinglist/model/MeasuringUnit.java
@@ -0,0 +1,11 @@
+package com.example.shoppinglist.model;
+
+public enum MeasuringUnit {
+    TSP,
+    TBSP,
+    CUP,
+    MILLILITRE,
+    LITRE,
+    GRAMS,
+    KILOGRAMS,
+}
