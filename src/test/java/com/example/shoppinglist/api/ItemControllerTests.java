6afe802d039f2c90a04c696bd8709e4db1e906a7
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-01 14:23:19 +0000

    added health check end point test

--- src/test/java/com/example/shoppinglist/api/ItemControllerTests.java
+++ src/test/java/com/example/shoppinglist/api/ItemControllerTests.java
@@ -1,4 +1,18 @@
 package com.example.shoppinglist.api;
 
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
+
+import io.restassured.matcher.RestAssuredMatchers.*;
+import org.hamcrest.Matchers.*;
+
 public class ItemControllerTests {
+    @Test
+    public void healthCheck() {
+        get("/actuator/health").then().statusCode(200).assertThat()
+                .body("status", equalTo("UP"));
+    }
+
 }
