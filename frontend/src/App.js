ed26212714c0bdcda396a4d6cc12a427609abd4e
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-03 18:45:13 +0000

    react-display list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,26 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-
-
 class App extends Component {
   state = {
-    clients: []
+    items: []
   };
 
   async componentDidMount() {
-    const response = await fetch('/clients');
+    const response = await fetch('/shoppinglist');
     const body = await response.json();
-    this.setState({clients: body});
+    this.setState({items: body});
   }
-
   render() {
-    const {clients} = this.state;
+    const {items} = this.state;
     return (
         <div className="App">
           <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <div className="App-intro">
-              <h2>Clients</h2>
-              {clients.map(client =>
-                  <div key={client.id}>
-                    {client.name} ({client.email})
+              <h2>Shopping List Items</h2>
+              {items.map(item =>
+                  <div key={item.id}>
+                    {item.name} {item.quantity}, {item.unit}
                   </div>
               )}
             </div>
