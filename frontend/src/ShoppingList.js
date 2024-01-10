f94a1edaa616aa92456ba78c9ca358041bd81579
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-10 07:31:46 +0000

    created shopping list component

--- frontend/src/ShoppingList.js
+++ frontend/src/ShoppingList.js
@@ -1,20 +1,39 @@
-import React, { Component } from 'react';
-import { Button, ButtonGroup, Container, Table } from 'reactstrap';
-import AppNavbar from './AppNavbar';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
 
-class ShoppingList extends Component {
+function ShoppingList() {
+    const [data , setData] = useState();
+    const [error , setError] = useState();
 
-    constructor(props) {
-        super(props);
-        this.state = {items: []};
-        this.remove = this.remove.bind(this);
-    }
 
-    componentDidMount() {
-        fetch('/shoppinglist')
-            .then(response => response.json())
-            .then(data => this.setState({items: data}));
-    }
+    const getData = async () => {
+        try {
+            const response = await fetch ('/shoppinglist');
+            if (response.ok) {
+                const jsonResponse = await response.json();
+                setData(jsonResponse);
+            }  else {
+            throw new Error('Request failed');
+            }
+        } catch (error) {
+            console.log(error);
+            setError(error.message);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <div>
+            {data ? (
+                <div>
+                <h2>Data from API:</h2>
+                <pre>{JSON.stringify(data, null, 2)}</pre>
+                </div>
+            ) : ( <p>Loading...</p>)}
+        </div>
+    );
 }
+
 export default ShoppingList;
