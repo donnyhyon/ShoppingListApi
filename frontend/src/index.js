c19a0f16f442b9fbf70728a9d1bff14944031fe1
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-26 07:27:27 +0000

    added bootstrap

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,7 +5,8 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import './index.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import routes from "./routes";
 
 
