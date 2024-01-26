3d68b09ca11b467c8898bba93332594b4a1fa9bc
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-26 07:45:03 +0000

    added bootstrap navbar

--- frontend/src/Components/NavBar/NavBar.js
+++ frontend/src/Components/NavBar/NavBar.js
@@ -1,1 +1,25 @@
-import NavBar
+
+
+function NavBar() {
+
+    return (
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Shopping List</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="/">Shopping List</a>
+                    <a class="nav-link" href="/Calendar">Calendar</a>
+                    <a class="nav-link" href="/Calories">Calories</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    )
+
+}
+
+export default NavBar;
--- frontend/src/Container/ShoppingListTableContainer.js
+++ frontend/src/Container/ShoppingListTableContainer.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { useLoaderData, redirect } from 'react-router-dom';
 import ShoppingListTable from '../Components/ShoppingListTable/ShoppingListTable';
+import NavBar from '../Components/NavBar/NavBar';
 
 
 function ShoppingListTableContainer() {
     const { items } = useLoaderData()
     return (
         <>
+            <NavBar />
             <ShoppingListTable items={items}/>
         </>
     );
