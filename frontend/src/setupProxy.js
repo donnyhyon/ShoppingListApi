e0b393014f252db92b79528e530942ca18d37b47
donnyhyon <dhyon333@hotmail.co.uk>
2024-01-28 07:39:52 +0000

    changed proxy to hit api backend

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/',
+    '/api/',
     createProxyMiddleware({
       target: `http://${process.env.REACT_APP_API_URL}:8080`,
       changeOrigin: true,
