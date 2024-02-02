beba30f76a774b32ad6040340cd932f06c716ecb
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-02 05:37:23 +0000

    tweaking url of transformation

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
     transformIgnorePatterns: [
-        "<rootDir>/node_modules/@testing-library/jest-dom/*)"
+        "<rootDir>/node_modules/@testing-library/*)"
       ],
     }
