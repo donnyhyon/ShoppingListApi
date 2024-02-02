9e0f0598423695042df17c81209ae801fd59ea3e
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-02 05:38:31 +0000

    removed trailing bracket

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
     transformIgnorePatterns: [
-        "<rootDir>/node_modules/@testing-library/*)"
+        "<rootDir>/node_modules/@testing-library/*"
       ],
     }
