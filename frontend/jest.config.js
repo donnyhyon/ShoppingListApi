54601de5108173b9a72a43142fbd29bdeff3edb1
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-02 05:40:03 +0000

    added adobe to transform

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     setupFilesAfterEnv: ["<rootDir>/src/setupTests.js"],
     transformIgnorePatterns: [
-        "<rootDir>/node_modules/@testing-library/*"
+        "<rootDir>/node_modules/@testing-library/*",
+        "<rootDir>/node_modules/@adobe/*"
       ],
     }
