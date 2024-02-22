a203403e07cd038a4aaa960914d4466a0ab20625
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-22 07:08:00 +0000

    refactored tests to add setup method

--- backend/src/test/java/com/example/shoppinglist/model/UserTest.java
+++ backend/src/test/java/com/example/shoppinglist/model/UserTest.java
@@ -1,2 +1,33 @@
-package com.example.shoppinglist.model;public class UserTest {
+package com.example.shoppinglist.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class UserTest {
+
+    private User user;
+
+    @BeforeEach
+    public void setUpNewUser(){
+        user = new User();
+    }
+
+    @Test
+    public void createUserClass(){
+
+        assertNotNull(user);
+    }
+
+    @Test
+    public void testId() {
+        Long idValue = 1L;
+
+        user.setId(idValue);
+
+        assertEquals(idValue, user.getId());
+    }
+
 }
