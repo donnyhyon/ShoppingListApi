690a9f35f55d6df746d9123dc2996648f2f5679a
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-23 06:45:48 +0000

    failing find by ID test

--- backend/src/test/java/com/example/shoppinglist/service/UserServiceTests.java
+++ backend/src/test/java/com/example/shoppinglist/service/UserServiceTests.java
@@ -1,10 +1,17 @@
 package com.example.shoppinglist.service;
 
+import com.example.shoppinglist.model.User;
 import com.example.shoppinglist.repository.UserRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
 
@@ -13,7 +20,27 @@ public class UserServiceTests {
     private UserService userService;
 
     @Mock
-    private UserRepository userRepository;
+    private UserRepository mockedUserRepository;
+
+    @Test
+    public void findUser(){
+        User user = new User();
+        user.setId(1L);
+        user.setUsername("testuser");
+        user.setFirstname("Testy");
+        user.setSurname("Testerson");
+        user.setPassword("secretPassWord");
+        user.setEmail("test@example.com");
+
+        when(mockedUserRepository.findById(1L)).thenReturn(Optional.of(user));
+
+        User result = userService.findById(1L);
+
+        assertEquals(user, result);
+
+
+
+    }
 
 
 
