b5f041437c8c881e04dfd9d404b8e7a9a2334f8b
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-23 07:41:10 +0000

    failing find all users tests

--- backend/src/test/java/com/example/shoppinglist/service/UserServiceTests.java
+++ backend/src/test/java/com/example/shoppinglist/service/UserServiceTests.java
@@ -1,5 +1,6 @@
 package com.example.shoppinglist.service;
 
+import com.example.shoppinglist.model.Item;
 import com.example.shoppinglist.model.User;
 import com.example.shoppinglist.repository.UserRepository;
 import org.junit.jupiter.api.Test;
@@ -11,6 +12,8 @@ import static org.mockito.Mockito.when;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,9 +40,33 @@ public class UserServiceTests {
         User result = userService.findById(1L);
 
         assertEquals(user, result);
+    }
 
+    @Test
+    public void findAllUsers(){
+        User user1 = new User();
+        user1.setId(1L);
+        user1.setUsername("testuser");
+        user1.setFirstname("Testy");
+        user1.setSurname("Testerson");
+        user1.setPassword("secretPassWord");
+        user1.setEmail("test@example.com");
 
+        User user2 = new User();
+        user2.setId(1L);
+        user2.setUsername("testuser");
+        user2.setFirstname("Testy");
+        user2.setSurname("Testerson");
+        user2.setPassword("secretPassWord");
+        user2.setEmail("test@example.com");
 
+        List<User> users = Arrays.asList(user1, user2);
+
+        when(mockedUserRepository.findAll()).thenReturn(users);
+
+        Iterable<User> result = userService.findAll();
+
+        assertEquals(users, result);
     }
 
 
