593342cd07191f62b64890cbb305b1789326d9bf
donnyhyon <dhyon333@hotmail.co.uk>
2024-02-23 07:04:04 +0000

    passing find by ID test

--- backend/src/main/java/com/example/shoppinglist/service/UserService.java
+++ backend/src/main/java/com/example/shoppinglist/service/UserService.java
@@ -1,4 +1,31 @@
 package com.example.shoppinglist.service;
 
+import com.example.shoppinglist.model.User;
+import com.example.shoppinglist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User findById(Long id) {
+        Optional<User> userOptional = this.userRepository.findById(id);
+
+        if (userOptional.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No user found with that ID");
+        }
+        return userOptional.get();
+    }
+
 }
