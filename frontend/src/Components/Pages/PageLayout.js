88c15c242a5a095a9cb14c8e40f7e470ff5948af
donnyhyon <dhyon333@hotmail.co.uk>
2024-03-19 05:46:05 +0000

    Added navbar to base component

---
+++ frontend/src/Components/Pages/PageLayout.js
@@ -0,0 +1,11 @@
+import NavBar from "../NavBar/NavBar"
+
+function PageLayout () {
+    return(
+        <>
+        <NavBar />
+        </>
+    )
+}
+
+export default PageLayout
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -3,7 +3,7 @@ import AddItemForm from "./Components/AddItemForm/AdditemForm";
 import addItemAction from "./Components/AddItemForm/addItemAction";
 import EditItemForm from "./Components/EditItemForm/EditItemForm";
 import editItemAction from "./Components/EditItemForm/editItemAction";
-import HomePage from "./Components/Pages/HomePage";
+import PageLayout from "./Components/Pages/PageLayout";
 import RecipePage from "./Components/Pages/Recipes/RecipePage";
 import ItemButtons from "./Components/ShoppingListTable/ShoppingListRow/ItemButtons";
 import deleteItemAction from "./Components/ShoppingListTable/ShoppingListRow/deleteItemAction";
@@ -12,44 +12,45 @@ import ShoppingListTableContainer from "./Container/ShoppingListTableContainer";
 import { Counter } from "./Components/Counter/Counter";
 
 const routes = [
-  {
-    
-    //With current architecture I cannot use a router method as this re-directs every time action is made. 
-    path: "/shoppinglist",
-    element: <ShoppingListTableContainer/>,
-    loader: shoppingListLoader,
-    children: [
-      {
-        path: "/shoppinglist/create",
-        element: <AddItemForm />,
-        action: addItemAction,
-        
-      },
-      {
-        path: "/shoppinglist/delete",
-        element: <ItemButtons />,
-        action: deleteItemAction,
-      },
-      {
-        path: "/shoppinglist/edit",
-        element: <EditItemForm />,
-        action: editItemAction,
-      },
-
-    ]
-  },
   { 
     path: "/",
-    element: <HomePage />,
+    element: <PageLayout />,
+    children: [
+      {
+        //With current architecture I cannot use a router method as this re-directs every time action is made. 
+        path: "/shoppinglist",
+        element: <ShoppingListTableContainer/>,
+        loader: shoppingListLoader,
+        children: [
+          {
+            path: "/shoppinglist/create",
+            element: <AddItemForm />,
+            action: addItemAction,
+            
+          },
+          {
+            path: "/shoppinglist/delete",
+            element: <ItemButtons />,
+            action: deleteItemAction,
+          },
+          {
+            path: "/shoppinglist/edit",
+            element: <EditItemForm />,
+            action: editItemAction,
+          },
+        ]
+      },
+      { 
+        path: "/recipes",
+        element: <RecipePage />,
+      },
+      { 
+        path: "/counter",
+        element: <Counter />,
+      },
+    ]
   },
-  { 
-    path: "/recipes",
-    element: <RecipePage />,
-  },
-  { 
-    path: "/counter",
-    element: <Counter />,
-  },
+
 ];
 
 export default routes;
